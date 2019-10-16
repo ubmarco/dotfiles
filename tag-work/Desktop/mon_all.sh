a8d79d3457a8e5b969733a0cc27eac558c740698
Marco Heinemann <marco.heinemann@useblocks.com>
2019-10-16 10:27:15 +0200

    Added sleep before running audio sink default

--- tag-work/Desktop/mon_all.sh
+++ tag-work/Desktop/mon_all.sh
@@ -12,5 +12,8 @@ xrandr --output eDP1 --mode 1920x1080 --pos 0x977 --output DP1 --mode 2560x1440 
 # Set panel to primary monitor (right)
 sh ~/Desktop/pan_prim.sh
 
+# Wait a while until dock sound device is ready
+sleep 15s
+
 # Set default audio sink to the dock station
 sh ~/.dotfiles/unlinked/set_audio_dock.sh
