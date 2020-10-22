6a05bbae3c38f50d213fce919662285da320ed93
Marco Heinemann <marco.heinemann@useblocks.com>
2020-10-22 11:29:20 +0200

    Added share ub_int to inotify

--- unlinked/inotify_nextcloud.sh
+++ unlinked/inotify_nextcloud.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-inotifywait -mrq -e create -e modify -e delete $HOME/share_c_level $HOME/share_project_files | while read file; do (notify-send "File udated: $file"&) done
+inotifywait -mrq -e create -e modify -e delete $HOME/share_c_level $HOME/share_project_files $HOME/share_ub_int | while read file; do (notify-send "File udated: $file"&) done
