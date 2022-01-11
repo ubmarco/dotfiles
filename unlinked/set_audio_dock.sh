a6c285dae49209cfe5172893de4f143e140cd0e1
Marco Heinemann <marco.heinemann@useblocks.com>
2022-01-11 14:39:11 +0100

    Updated set default audio for dock

--- unlinked/set_audio_dock.sh
+++ unlinked/set_audio_dock.sh
@@ -1,1 +1,1 @@
-pactl set-default-sink alsa_output.usb-Burr-Brown_from_TI_USB_audio_CODEC-00.iec958-stereo
+pactl set-default-sink alsa_output.usb-Burr-Brown_from_TI_USB_audio_CODEC-00.analog-stereo
