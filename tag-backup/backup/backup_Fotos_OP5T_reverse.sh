c46c131f723c53293907657900fa1e18574f4375
Marco Heinemann <marco.heinemann@useblocks.com>
2022-09-14 11:29:43 +0200

    Removed OP5T backups

--- tag-backup/backup/backup_Fotos_OP5T_reverse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-src="/mnt/data/Sync/Fotos/Fotos_OP5T/"
-dst="root@192.168.2.110:/storage/emulated/0/DCIM/"
-cmd="rsync -e 'sshpass -p admin ssh -p2222 -oHostKeyAlgorithms=+ssh-dss -oKexAlgorithms=+diffie-hellman-group1-sha1' -hvrl --size-only --progress --delete --stats --exclude=.thumbnails --exclude=thumbnails --exclude=.inflight_lowres"
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-/bin/bash "${SCRIPT_DIR}/backup_base.sh" $@ -s $src -d $dst -c "$cmd"
