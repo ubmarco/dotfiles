136f163061438a7e994f3b5e8106eb64fc899b89
Marco Heinemann <marco.heinemann@useblocks.com>
2023-03-03 08:37:51 +0100

    Remove leading space

--- unlinked/wifi_stats.sh
+++ unlinked/wifi_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 WIFI_DEV=$(nmcli | rg Stockmann | awk -F ':' '{print $1}' | head -1)
 if [ -n "$WIFI_DEV" ]; then
-    iwlist $WIFI_DEV channel | rg Current
+    iwlist $WIFI_DEV channel | rg Current | sed 's/^[[:space:]]*//'
     echo
     iw dev $WIFI_DEV link
 fi
