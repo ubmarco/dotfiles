48d59f7bef10761d8713a1cda9b7b42919e7a30f
Marco Heinemann <marco.heinemann@useblocks.com>
2023-11-06 12:17:57 +0100

    Update inotify shares

--- unlinked/inotify_nextcloud.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-inotifywait -mrq -e create -e modify -e delete $HOME/share_c_level $HOME/share_project_files $HOME/share_ub_int | while read file; do (notify-send "File udated: $file"&) done
