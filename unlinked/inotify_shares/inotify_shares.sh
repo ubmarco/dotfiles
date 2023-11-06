0b4c6f203a3696a7a55acf5c8dc8e6cf313ff8de
Marco Heinemann <marco.heinemann@useblocks.com>
2023-11-06 12:20:52 +0100

    Remove grepping

--- unlinked/inotify_shares/inotify_shares.sh
+++ unlinked/inotify_shares/inotify_shares.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-inotifywait -mrq -e create -e modify -e delete $HOME/share_c_level $HOME/share_p_level $HOME/share_project_files $HOME/share_ub_int | grep -v -x -F -f $HOME/.dotfiles/unlinked/inotify_shares/ignore_list.txt | while read file; do (notify-send "File udated" "$file"&) done
+inotifywait -mrq -e create -e modify -e delete $HOME/share_c_level $HOME/share_p_level $HOME/share_project_files $HOME/share_ub_int | while read file; do (notify-send "File udated" "$file"&) done
