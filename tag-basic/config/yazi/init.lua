b23410e68d6b5aba2e7be3e667e780b21decccf3
Marco Heinemann <marco.heinemann@useblocks.com>
2025-05-15 14:45:09 +0200

    fmt

--- tag-basic/config/yazi/init.lua
+++ tag-basic/config/yazi/init.lua
@@ -4,14 +4,11 @@ require("bunny"):setup({
 		{ key = { "t", "r" }, path = "/tmp", },
 		{ key = { "t", "p" }, path = "/tmp/pytest-of-marco/pytest-current", },
 		{ key = "h",          path = "~",                                      desc = "Home" },
-		{ key = "m",          path = "~/Music",                                desc = "Music" },
 		{ key = { "n", "u" }, path = "/mnt/dlink_nas", },
 		{ key = { "n", "h" }, path = "/mnt/dlink_nas_home", },
 		{ key = "d",          path = "~/Downloads",                            desc = "Desktop" },
-		{ key = { "s", "r" }, path = "~/share_c_level", },
-		{ key = { "s", "b" }, path = "~/share_c_level/00_orga/03_Buchhaltung", },
-		{ key = { "l", "s" }, path = "~/.local/share",                         desc = "Local share" },
-		{ key = { "l", "b" }, path = "~/.local/bin",                           desc = "Local bin" },
+		{ key = { "s", "r" }, path = "~/share_c_level",                        desc = "share_c_level" },
+		{ key = { "s", "b" }, path = "~/share_c_level/00_orga/03_Buchhaltung", desc = "share_c_level / Buchhaltung" },
 		-- key and path attributes are required, desc is optional
 	},
 	desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
