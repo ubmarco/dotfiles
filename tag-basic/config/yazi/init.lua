d7840d7705fd077555fb1e6cf763c54ed38fcf8b
Marco Heinemann <marco.heinemann@useblocks.com>
2025-06-05 15:31:11 +0200

    Refactor yazi configuration: reorder hops for improved organization

--- tag-basic/config/yazi/init.lua
+++ tag-basic/config/yazi/init.lua
@@ -1,18 +1,18 @@
 require("bunny"):setup({
 	hops = {
 		{ key = "/",          path = "/", },
-		{ key = { "t", "r" }, path = "/tmp", },
-		{ key = { "t", "p" }, path = "/tmp/pytest-of-marco/pytest-current", },
-		{ key = "h",          path = "~",                                           desc = "Home" },
-		{ key = { "m", "u" }, path = "/mnt/dlink_nas", },
-		{ key = { "m", "h" }, path = "/mnt/dlink_nas_home", },
 		{ key = "d",          path = "~/Downloads",                                 desc = "Downloads" },
-		{ key = { "s", "r" }, path = "~/share_c_level",                             desc = "share_c_level" },
-		{ key = { "s", "b" }, path = "~/share_c_level/00_orga/03_Buchhaltung/2025", desc = "share_c_level / Buchhaltung" },
-		{ key = { "n", "c" }, path = "~/Nextcloud/ub_c_level",                      desc = "Nextcloud / ub_c_level" },
+		{ key = "h",          path = "~",                                           desc = "Home" },
 		{ key = { "f" },      path = "/mnt/data/Sync/Fotos", },
+		{ key = { "m", "h" }, path = "/mnt/dlink_nas_home", },
+		{ key = { "m", "u" }, path = "/mnt/dlink_nas", },
+		{ key = { "n", "c" }, path = "~/Nextcloud/ub_c_level",},
+		{ key = { "n", "r" }, path = "~/Nextcloud",},
+		{ key = { "s", "b" }, path = "~/share_c_level/00_orga/03_Buchhaltung/2025", desc = "share_c_level / Buchhaltung" },
+		{ key = { "s", "r" }, path = "~/share_c_level",                             desc = "share_c_level" },
+		{ key = { "t", "p" }, path = "/tmp/pytest-of-marco/pytest-current", },
+		{ key = { "t", "r" }, path = "/tmp", },
 		{ key = { "v", "b" }, path = "/home/marco/Videos/BBT", },
-
 		-- key and path attributes are required, desc is optional
 	},
 	desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
